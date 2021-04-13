cac700d66b69514a5a42595bb7b95d76285aa484
JoaoCumbula <joaocumbula@hotmail.com>
2021-04-13 23:18:14 +0200

    Add total count for dashboard

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -1,12 +1,31 @@
-function getCountFarmacia(ref) {
-  // Sum the count of each shard in the subcollection
-  return ref.collectionGroup("farmacia").collectionGroup((snapshot) => {
-    let total_count = 0;
-    snapshot.forEach((doc) => {
-      total_count += doc.data().count;
-    });
+const db = firebase.firestore();
 
-    return total_count;
-  });
-}
+const categoria = document.getElementById("categoria");
+const farmacia = document.getElementById("farmacia");
+const gestor = document.getElementById("gestor");
+const produto = document.getElementById("produto");
+
+db.collectionGroup("farmacia").get().then(snap => {
+   let size = snap.size;
+  console.log("The size is = "+size);
+  farmacia.innerHTML = size;
+});
+
+db.collectionGroup("usuario").get().then(snap => {
+  let size = snap.size;
+ console.log("The size is = "+size);
+ gestor.innerHTML = size;
+});
+
+db.collectionGroup("categoria").get().then(snap => {
+  let size = snap.size;
+ console.log("The size is = "+size);
+ categoria.innerHTML = size;
+});
+
+db.collectionGroup("produto").get().then(snap => {
+  let size = snap.size;
+ console.log("The size is = "+size);
+ produto.innerHTML = size;
+});
 
